d03fe966f3dad11093ad922e2ad241172f64a55f
陈宏铭 <91675576+ChenHongMing666@users.noreply.github.com>
2021-10-03 08:48:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from random import randint, choice
 import pygame
 from sys import exit
+from time import sleep
 
 
+pygame.init()
 class 扫雷:
-    def __init__(self, 地雷数=10, 地图大小=5) :
+
+    def __init__(self, 地雷数=5, 地图大小=5) :
         self.hand_x = 0
         self.hand_y = 0
         self.live = True
@@ -13,15 +16,15 @@ class 扫雷:
         self.MapSize = 地图大小
         if self.tnts > self.MapSize**2 :
             self.tnts = self.MapSize**2*2//5
-        self.map_out = [ [ '■' for i in range(self.MapSize) ] for j in range(self.MapSize) ]
-        self.map_book = [ [ 0 for i1 in range(self.MapSize) ] for j1 in range(self.MapSize) ]
+        self.map_out = [ [ '■' for i in range(self.MapSize+1) ] for j in range(self.MapSize+1) ]
+        self.map_book = [ [ 0 for i1 in range(self.MapSize+1) ] for j1 in range(self.MapSize+1) ]
         self.map_tab = self.map_book.copy()
         self.next = [[0,1],[1,0],[0,-1],[-1,0],[1,1],[1,-1],[-1,1],[-1,-1]]
         while self.tnt_rest <= self.tnts:
-            x = randint(0,5)
-            y = randint(0,5)
+            x = randint(0,self.MapSize-1)
+            y = randint(0,self.MapSize-1)
             if self.map_book[x][y] != 9 :
-                self.map_book[x][y] = 9
+                self.map_book[x][y] > 9
                 for k in self.next:
                     tx = x + k[0]
                     ty = y + k[1]
@@ -53,16 +56,14 @@ class 扫雷:
     def win(self):
         for i4 in range(self.MapSize):
             for j4 in range(self.MapSize):
-                if self.map_book == 9 and self.map_out == "■" or self.map_out == "+":
+                if self.map_book == 9 and self.map_out == "■":
                     print('you win !')
     
     def move(self):
         for events in pygame.event.get():
-            if events.type == pygame.QUIT:
-                exit()
-            elif events.type == pygame.KEYDOWN:
+            if  events.type == pygame.KEYDOWN:
                 if events.key == pygame.K_UP:
-                    if self.hand_y < 1:
+                    if self.hand_y == 0:
                         pass
                     else:
                         self.hand_y -= 1
@@ -72,7 +73,7 @@ class 扫雷:
                     else:
                         self.hand_y += 1
                 elif events.key == pygame.K_LEFT:
-                    if self.hand_x < 1:
+                    if self.hand_x == 0:
                         pass
                     else:
                         self.hand_x -= 1
@@ -87,5 +88,28 @@ class 扫雷:
                     else:
                         self.dfs(self.hand_x,self.hand_y)
             self.map_out[self.hand_y][self.hand_x] = '□'
+            for y in range(self.MapSize):
+                for x in range(self.MapSize):
+                    print(self.map_out[y][x],end=' ')
+                print()
+
     def main_game(self):
-        pass
+        while self.live :
+            self.move()
+            self.win()
+
+
+if __name__ == "__main__" :
+    size =  input('size:')
+    tnts = input('tnt:')
+    if size :
+        if tnts :
+            try:
+                ai = 扫雷(int(tnts),int(size))
+            except ValueError:
+                ai = 扫雷()
+                ai.main_game()
+            finally :
+                print('game start!')
+                ai.main_game()
+            
